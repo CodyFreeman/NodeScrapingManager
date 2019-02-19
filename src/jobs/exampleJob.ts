9a157c5bb2eef65a43bb181e8d563ca379f3fb10
Cody Freeman <codycodescode@gmail.com>
2019-02-19 15:58:25 +0100

    Updates example job

--- src/jobs/exampleJob.ts
+++ src/jobs/exampleJob.ts
@@ -7,27 +7,53 @@ const RESULT_PATH = `/exampleResults`;
 
 const uuid = `94a6ecfb-7d7c-4d12-9854-9cea3b940692`;
 const name = `example job`;
-const interval = 3600;
-const puppeteerConfig = { headless: false};
+const interval = 3;
+const puppeteerConfig = {
+    headless: false,
+    args: ['--window-size=1600,900'],
+    defaultViewport: { width: 1600, height: 900 }
+};
 
 async function before(repo: JobRepo) {
     return repo.load(RESULT_PATH);
 }
 
 async function main(repo: JobRepo, browser: Browser, beforeResult: any) {
-    return { time: Math.floor(new Date().getTime() / 1000), success: true };
+
+    const page = await browser.newPage();
+    await page.goto(`https://duckduckgo.com`);
+    await page.type(`#search_form_input_homepage`, `what is the answer to life the universe and everything`);
+
+    await Promise.all([
+        page.click(`#search_button_homepage`, {}),
+        page.waitForNavigation()
+    ]);
+
+    const urls = await page.$$eval(`.result__url`, (elements) => {
+        return elements.map((element) => {
+
+            const domain = element.querySelector(`.result__url__domain`);
+            const path = element.querySelector(`.result__url__full`);
+
+            if (domain && path && `innerHTML` in domain && `innerHTML` in path) {
+                return domain.innerHTML + path.innerHTML;
+            }
+
+        });
+    });
+    await page.screenshot({ path: `exampleJob.png` });
+    await page.goto(`https://www.youtube.com/watch?v=dQw4w9WgXcQ&feature=youtu.be&t=42`);
+    await new Promise((resolve) => {
+        setTimeout(resolve, 10000);
+    });
+    return await urls;
 }
 
 async function after(repo: JobRepo, mainResult: any) {
-
-    if (!mainResult || !mainResult.success) {
-        return false;
-    }
-
     repo.save(`${RESULT_PATH}[]`, mainResult);
-
     return true;
 }
+
 const jobConfig = JobConfigFactory.createConfig(interval, puppeteerConfig, 60, 2);
 const job = JobFactory.createJob(uuid, name, jobConfig, before, main, after);
 
